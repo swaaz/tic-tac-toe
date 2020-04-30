65bf01fba3ee1cde8aa9fe508b2654a9c930f099
swaaz <swaasthik.shetty07@gmail.com>
2020-04-30 14:17:13 +0530

    bug fixed

--- single_player.py
+++ single_player.py
@@ -4,7 +4,7 @@ x_count = 0
 o_count = 0
 s_count = 0
 game_still_on = True
-current_player = "X"
+current_player = "O"
 #initializing list to empty( _ )
 list = ["_" ,"_" ,"_" ,"_" ,"_" ,"_" ,"_" ,"_" ,"_" ]
 #function defination
@@ -56,8 +56,8 @@ def get_input(current_player): #function to take the index input
       print("Invalid Input!!!!")
       get_input(current_player)
   else:                        #if current player in O
-    index = random.randint(1,10) #generates random function from 1-9
-    index = index - 1 
+    index = O_turn()
+    print(index)
     if list[index] != "_" : #checking whether the index is empty
       get_input(current_player)
     else:
@@ -77,7 +77,146 @@ def play_game(): #function which run the game
     count()
     check()
     switch_player()
+def O_turn():
+  count()
+  if o_count == 2:
+    if list[0] == list[1] or list[1] == list[2] or list[0] == list[2]: #checking first row
+        if list[0] == list[1]:
+          if list[2] != "_":
+            return random.randint(0,8)
+          else:
+            return 2
+        elif list[1] == list[2]:
+          if list[0] != "_":
+            return random.randint(0,8)
+          else:
+            return 0
+        elif list[0] == list[2]:
+          if list[1] != "_":
+            return random.randint(0,8)
+          else:
+            return 1
+    elif  list[3] == list[4] or list[4] == list[5] or list[3] == list[5]: #checking second row
+        if list[3] == list[4]:
+          if list[5] != "_":
+            return random.randint(0,8)
+          else:
+            return 5
+        elif list[4] == list[5]:
+          if list[3] != "_":
+            return random.randint(0,8)
+          else:
+            return 3
+        elif list[3] == list[5]:
+          if list[4] != "_":
+            return random.randint(0,8)
+          else:
+            return 4
+    elif list[6] == list[7] or list[7] == list[8] or list[6] == list[8]: #checking thrid row
+        if list[6] == list[7]:
+          if list[8] != "_":
+            return random.randint(0,8)
+          else:
+            return 8
+        elif list[7] == list[8]:
+          if list[6] != "_":
+            return random.randint(0,8)
+          else:
+            return 6
+        elif list[6] == list[8]:
+          if list[7] != "_":
+            return random.randint(0,8)
+          else:
+            return 7
+    elif list[0] == list[3] or list[3] == list[6] or list[0] == list[6]: #checking first column
+        if list[0] == list[3]:
+          if list[6] != "_":
+            return random.randint(0,8)
+          else:
+            return 6
+        elif list[3] == list[6]:
+          if list[0] != "_":
+            return random.randint(0,8)
+          else:
+            return 0
+        elif list[0] == list[6]:
+          if list[3] != "_":
+            return random.randint(0,8)
+          else:
+            return 3
+    elif list[1] == list[4] or list[4] == list[7] or list[1] == list[7]: #checking 2nd column
+        if list[1] == list[4]:
+          if list[7] != "_":
+            return random.randint(0,8)
+          else:
+            return 7
+        elif list[4] == list[7]:
+          if list[1] != "_":
+            return random.randint(0,8)
+          else:
+            return 1
+        elif list[1] == list[7]:
+          if list[4] != "_":
+            return random.randint(0,8)
+          else:
+            return 4
+    elif list[2] == list[5] or list[5] == list[8] or list[2] == list[8]: #checking third column
+        if list[2] == list[5]:
+          if list[8] != "_":
+            return random.randint(0,8)
+          else:
+            return 8
+        elif list[5] == list[8]:
+          if list[2] != "_":
+            return random.randint(0,8)
+          else:
+            return 2
+        elif list[2] == list[8]:
+          if list[5] != "_":
+            return random.randint(0,8)
+          else:
+            return 5
+    elif list[0] == list[4] or list[4] == list[8] or list[0] == list[8]: #checking diagonal
+        if list[0] == list[4]:
+          if list[8] != "_":
+            return random.randint(0,8)
+          else:
+            return 8
+        elif list[4] == list[8]:
+          if list[0] != "_":
+            return random.randint(0,8)
+          else:
+            return 0
+        elif list[0] == list[8]:
+          if list[4] != "_":
+            return random.randint(0,8)
+          else:
+            return 4
+    elif list[2] == list[4] or list[4] == list[6] or list[2] == list[6]: #checking diagonal
+        if list[2] == list[4]:
+          if list[6] != "_":
+            return random.randint(0,8)
+          else:
+            return 6
+        elif list[4] == list[6]:
+          if list[2] != "_":
+            return random.randint(0,8)
+          else:
+            return 2
+        elif list[2] == list[6]:
+          if list[4] != "_":
+            return random.randint(0,8)
+          else:
+            return 4
+    else:
+      return random.randint(0,8)
+  elif o_count == 1 :
+    return random.randint(0,8)
+  else:
+    return random.randint(0,8)
 
+
+      
 
 #function call
 play_game()
